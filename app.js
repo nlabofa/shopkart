d4cd3733e9bc9cef32918c6387b32aa110cff257
Opeyemi Agbeja <niyola59@gmail.com>
2018-02-21 17:59:44 +0100

    fixed login/logout user session, fixed file path issh

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.set('views', path.join(__dirname, 'views')); // this is the folder where we 
 app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
 
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use("/public",express.static(path.join(__dirname, 'public')));
 
 // Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
